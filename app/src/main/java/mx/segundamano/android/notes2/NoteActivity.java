d8a63165f864a3b485d43c5e287386fd4f421d89
Juan Pablo "Gianpa" Villa <pablo.villa@schibsted.com.mx>
2016-11-11 15:16:25 -0600

    User Interface

--- app/src/main/java/mx/segundamano/android/notes2/NoteActivity.java
+++ app/src/main/java/mx/segundamano/android/notes2/NoteActivity.java
@@ -2,12 +2,25 @@ package mx.segundamano.android.notes2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class NoteActivity extends AppCompatActivity {
+
+    private EditText noteEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.note_activity);
+
+        noteEditText = (EditText) findViewById(R.id.note_edit_text);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.notes_activity_menu, menu);
+        return true;
     }
 }
