23213f192c879a879d31f024f9cc7aaf8f783bfd
Juan Pablo "Gianpa" Villa <pablo.villa@schibsted.com.mx>
2016-11-11 15:24:50 -0600

    Saving in Realm

--- app/src/main/java/mx/segundamano/android/notes2/NoteActivity.java
+++ app/src/main/java/mx/segundamano/android/notes2/NoteActivity.java
@@ -6,10 +6,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import io.realm.Realm;
 
 public class NoteActivity extends AppCompatActivity {
 
     private EditText noteEditText;
+    private Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,8 @@ public class NoteActivity extends AppCompatActivity {
         setContentView(R.layout.note_activity);
 
         noteEditText = (EditText) findViewById(R.id.note_edit_text);
+
+        realm = Realm.getDefaultInstance();
     }
 
     @Override
@@ -28,7 +34,24 @@ public class NoteActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == R.id.note_action_save)  {
-
+            final NoteRealm noteRealm = new NoteRealm();
+            noteRealm.note = noteEditText.getText().toString();
+            realm.executeTransactionAsync(new Realm.Transaction() {
+                @Override
+                public void execute(Realm bgRealm) {
+                    bgRealm.insertOrUpdate(noteRealm);
+                }
+            }, new Realm.Transaction.OnSuccess() {
+                @Override
+                public void onSuccess() {
+                    Toast.makeText(NoteActivity.this, "Saved!", Toast.LENGTH_SHORT).show();
+                }
+            }, new Realm.Transaction.OnError() {
+                @Override
+                public void onError(Throwable error) {
+                    Toast.makeText(NoteActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
 
         return super.onOptionsItemSelected(item);
